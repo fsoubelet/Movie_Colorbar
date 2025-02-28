ea3cb0ceaf90baa72be0a706286cf409aff59634
Felix Soubelet <felix.soubelet@protonmail.com>
2025-02-28 19:19:38 +0100

    skeleton to replace most of generative_functions

---
+++ new/image.py
@@ -0,0 +1,11 @@
+"""
+Image
+-----
+
+Small module with different functions to handle parsing
+of images and extracting color information from them.
+"""
+
+from PIL import image
+
+from new.colors import *
