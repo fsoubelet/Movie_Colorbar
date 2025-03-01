aa907bd98e71f046fc7ca94e9aab67a66d8f8b8c
Felix Soubelet <felix.soubelet@protonmail.com>
2025-03-01 12:42:56 +0100

    clearer and slightly faster version of avg hsv

--- new/image.py
+++ new/image.py
@@ -9,7 +9,7 @@ of images and extracting color information from them.
 from loguru import logger
 from PIL import Image
 
-from new.colors import *
+from new.colors import convert_hsv_to_rgb, convert_rgb_to_hsv
 
 
 def get_rgb_counts_and_colors(image: Image) -> list[tuple[int, tuple[int, int, int]]]:
@@ -103,3 +103,42 @@ def get_average_rgb_squared(image: Image) -> tuple[int, int, int]:
     return int(avg_r2**0.5), int(avg_g2**0.5), int(avg_b2**0.5)
 
 
+def get_average_hsv(image: Image) -> tuple[int, int, int]:
+    """
+    Get the average H, S and V values of the colors in an image,
+    as an RGB color to be displayed.
+
+    Parameters
+    ----------
+    image : PIL.Image
+        The image to extract the colors from.
+
+    Returns
+    -------
+    tuple[float, float, float]
+        A tuple with the R, G, B values corresponding to the
+        average HSV color of the image.
+    """
+    counts_and_colors = get_rgb_counts_and_colors(image)
+    logger.trace("Computing average HSV components of the image")
+    total_pixels = 0
+    total_h = 0
+    total_s = 0
+    total_v = 0
+
+    for count, (r, g, b) in counts_and_colors:
+        # Get HSV values from RGB - colorsys wants RGB in [0, 1] range
+        h, s, v = convert_rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)
+        total_pixels += count
+        total_h += count * h
+        total_s += count * s
+        total_v += count * v
+
+    avg_h = total_h / total_pixels
+    avg_s = total_s / total_pixels
+    avg_v = total_v / total_pixels
+
+    # Get the corresponding RGB values for the average HSV color and
+    # scale them back to [0, 255] range (colorsys works in [0, 1])
+    avg_r, avg_g, avg_b = convert_hsv_to_rgb(avg_h, avg_s, avg_v)
+    return int(avg_r * 255), int(avg_g * 255), int(avg_b * 255)
