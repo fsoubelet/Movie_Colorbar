6a6f4bf3092c53f9f6447b5af8ef5f99650778c2
Felix Soubelet <felix.soubelet@protonmail.com>
2025-03-01 12:09:10 +0100

    version for average RGB, explained and clearer now

--- new/image.py
+++ new/image.py
@@ -6,12 +6,13 @@ Small module with different functions to handle parsing
 of images and extracting color information from them.
 """
 
-from PIL import Image
 from loguru import logger
+from PIL import Image
+
 from new.colors import *
 
 
-def get_rgb_colors_and_counts(image: Image) -> list[tuple[int, tuple[int, int, int]]]:
+def get_rgb_counts_and_colors(image: Image) -> list[tuple[int, tuple[int, int, int]]]:
     """
     Get the RGB colors of an image.
 
@@ -31,3 +32,37 @@ def get_rgb_colors_and_counts(image: Image) -> list[tuple[int, tuple[int, int, i
     img_rgb = image.convert("RGB")
     return img_rgb.getcolors(img_rgb.size[0] * img_rgb.size[1])
 
+
+def get_average_rgb(image: Image) -> tuple[float, float, float]:
+    """
+    Get the average R, G and B values of the colors in an image.
+    The values are weighted by the number of pixels of each color.
+
+    Parameters
+    ----------
+    image : PIL.Image
+        The image to extract the colors from.
+
+    Returns
+    -------
+    tuple[float, float, float]
+        A tuple with the average R, G and B values of the image.
+    """
+    counts_and_colors = get_rgb_counts_and_colors(image)
+
+    total_pixels = 0
+    total_r = 0
+    total_g = 0
+    total_b = 0
+
+    for count, (r, g, b) in counts_and_colors:
+        total_pixels += count
+        total_r += count * r
+        total_g += count * g
+        total_b += count * b
+
+    avg_r = total_r / total_pixels
+    avg_g = total_g / total_pixels
+    avg_b = total_b / total_pixels
+
+    return int(avg_r), int(avg_g), int(avg_b)
