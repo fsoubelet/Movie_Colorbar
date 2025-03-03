90f2079e2f8be5a3e8d80df5531fb535e364e174
Felix Soubelet <felix.soubelet@protonmail.com>
2025-03-03 11:56:05 +0100

    export constants here

---
+++ new/constants.py
@@ -0,0 +1,52 @@
+"""
+Constants
+---------
+
+Module with different constants used in the project.
+"""
+
+# ----- Methods ----- #
+
+RGB: str = "rgb"
+HSV: str = "hsv"
+HUE: str = "hue"
+KMEANS: str = "kmeans"
+COMMON: str = "common"
+XYZ: str = "xyz"
+LAB: str = "lab"
+RGB_SQUARED: str = "rgbsquared"
+RESIZE: str = "resize"
+QUANTIZED: str = "quantized"
+
+
+# ----- Extensions ----- #
+
+VALID_VIDEO_EXTENSIONS: tuple[str, ...] = (
+    ".webm",
+    ".mkv",
+    ".flv",
+    ".vob",
+    ".ogg",
+    ".ogv",
+    ".drc",
+    ".gif",
+    ".gifv",
+    ".mng",
+    ".avi",
+    ".mov",
+    ".qt",
+    ".wmv",
+    ".yuv",
+    ".rm",
+    ".rmvb",
+    ".asf",
+    ".amv",
+    ".mp4",
+    ".m4p",
+    ".m4v",
+    ".mpg",
+    ".mp2",
+    ".mpv",
+    ".m4v",
+    ".flv",
+)
