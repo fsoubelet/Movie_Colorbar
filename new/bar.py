4e272bed00fba59810fd6f4669aeaa5f6ec9242f
Felix Soubelet <felix.soubelet@protonmail.com>
2025-03-05 00:48:57 +0100

    propagate here

--- new/bar.py
+++ new/bar.py
@@ -19,18 +19,7 @@ try:
 except ImportError:
     JOBLIB_AVAILABLE: bool = False
 
-from new.constants import (
-    COMMON,
-    HSV,
-    HUE,
-    KMEANS,
-    LAB,
-    QUANTIZED,
-    RESIZE,
-    RGB,
-    RGB_SQUARED,
-    XYZ,
-)
+from new.constants import Methods
 from new.image import (
     get_average_hsv_as_rgb,
     get_average_hue_as_rgb,
@@ -47,16 +36,16 @@ from new.image import (
 # ----- Mapping methods to called function ----- #
 
 METHOD_ACTION_MAP: dict = {
-    COMMON: get_most_common_color_as_rgb,
-    HSV: get_average_hsv_as_rgb,
-    HUE: get_average_hue_as_rgb,
-    KMEANS: get_kmeans_color_as_rgb,
-    LAB: get_average_lab_as_rgb,
-    QUANTIZED: get_quantized_color_as_rgb,
-    RESIZE: get_resized_1px_rgb,
-    RGB: get_average_rgb,
-    RGB_SQUARED: get_average_rgb_squared,
-    XYZ: get_average_xyz_as_rgb,
+    Methods.common: get_most_common_color_as_rgb,
+    Methods.hsv: get_average_hsv_as_rgb,
+    Methods.hue: get_average_hue_as_rgb,
+    Methods.kmeans: get_kmeans_color_as_rgb,
+    Methods.lab: get_average_lab_as_rgb,
+    Methods.quantized: get_quantized_color_as_rgb,
+    Methods.resize: get_resized_1px_rgb,
+    Methods.rgb: get_average_rgb,
+    Methods.rgb_squared: get_average_rgb_squared,
+    Methods.xyz: get_average_xyz_as_rgb,
 }
 
 # ----- Functions to Turn Create Colorbars ----- #
